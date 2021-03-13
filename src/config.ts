beb836d1183c09191adf4f99e9cec63f431845cf
Mery <maria.juan.vi@gmail.com>
2021-03-13 16:28:34 +0100

    Organización codigo
    Preparado para nivel 1

---
+++ src/config.ts
@@ -0,0 +1,10 @@
+import Nivel1 from "./escenas/nivel1";
+
+const config = {
+    type: Phaser.AUTO,
+    backgroundColor: '#125555',
+    width: 800,
+    height: 600,
+    scene: Nivel1
+};
+export default config;
