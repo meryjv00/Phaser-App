5fe05279da9d8cb050f7e6f30c869c8630732664
Mery <maria.juan.vi@gmail.com>
2021-03-13 15:16:53 +0100

    Preparación inicial juego

---
+++ v00/dist/js/holamundo.js
@@ -0,0 +1,4 @@
+var mensaje = "Desarrollo de Videojuegos con Phaser ";
+var version = 3;
+var mensaje2 = " y TypeScript";
+console.log(mensaje + version + mensaje2);
---
+++ v00/src/holamundo.ts
@@ -0,0 +1,4 @@
+let mensaje: string = "Desarrollo de Videojuegos con Phaser ";
+let version: number = 3;
+let mensaje2: string = " y TypeScript";
+console.log(mensaje + version + mensaje2);
